0c8a76e268e407ae04a27efe48a36bbbb9e504c2
Jordan Judt <dev@jordanjudt.com>
2020-03-21 14:15:11 -0500

    modify intents index to be dynamic

--- src/alexa/handlers/requests/intents/index.js
+++ src/alexa/handlers/requests/intents/index.js
@@ -1,6 +1,13 @@
-module.exports = [
-    require("./CancelStop"),
-    require("./Help"),
+const fs = require("fs");
+const path = require('path');
 
-    require("./HelloWorld"),
-];
+const exclude = [path.basename(__filename), "util.js"];
+const req = [];
+
+const files = fs.readdirSync("./");
+for (let f in files) {
+    if (!exclude.includes(f) && path.extname(f) === ".js")
+        req.push(require(`./${f}`));
+}
+
+module.exports = req;
