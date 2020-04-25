061bb10b855337034e36e5b62d208e634b7eb4f5
Jordan Judt <dev@jordanjudt.com>
2020-04-25 18:06:23 -0500

    add HelloWorldIntent response test

--- test/alexa/handlers/requests/intents/HelloWorld.spec.js
+++ test/alexa/handlers/requests/intents/HelloWorld.spec.js
@@ -1,6 +1,39 @@
+const { assert } = require("chai");
+const sinon = require("sinon");
 const { exportsHandlerTest, getFile } = require("./util");
 const handler = getFile(__filename);
 
 describe("HelloWorldIntent", function () {
+    let input = { responseBuilder: {} }, responseBuilder;
+
+    beforeEach(() => {
+        responseBuilder = {};
+        responseBuilder.speak = sinon.fake.returns(responseBuilder);
+        responseBuilder.withSimpleCard = sinon.fake.returns(responseBuilder);
+        responseBuilder.getResponse = sinon.fake.returns(true);
+
+        input.responseBuilder = responseBuilder;
+    });
+
     it("exports handler", exportsHandlerTest(handler));
+
+    describe("returns hello world", () => {
+        it("speech response", async () => {
+            await handler.handle(input);
+            assert.isTrue(responseBuilder.speak.calledOnce, "Speak not called once");
+            assert.equal(responseBuilder.speak.args[0][0], "Hello World!", "Unexpected returned speech");
+        });
+
+        it("card title", async () => {
+            await handler.handle(input);
+            assert.isTrue(responseBuilder.withSimpleCard.calledOnce, "withSimpleCard not called once");
+            assert.equal(responseBuilder.withSimpleCard.args[0][0], "Hello World", "Unexpected card title");
+        });
+
+        it("card body", async () => {
+            await handler.handle(input);
+            assert.isTrue(responseBuilder.withSimpleCard.calledOnce, "withSimpleCard not called once");
+            assert.equal(responseBuilder.withSimpleCard.args[0][1], "Hello World!", "Unexpected card body");
+        });
+    });
 });
